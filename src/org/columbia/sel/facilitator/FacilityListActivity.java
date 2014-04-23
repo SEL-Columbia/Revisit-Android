743ad9dae6c1b98cfcdfa8578376dfd3f098564b
Jonathan Wohl <jon@jonwohl.com>
2014-04-23 17:30:11 -0400

    Added location awareness, list now shows only facilities within 1 mile

--- src/org/columbia/sel/facilitator/FacilityListActivity.java
+++ src/org/columbia/sel/facilitator/FacilityListActivity.java
@@ -1,27 +1,20 @@
 package org.columbia.sel.facilitator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import org.columbia.sel.facilitator.model.Facility;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
-import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-
-import org.columbia.sel.facilitator.model.Facility;
-
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.AsyncTask;
@@ -30,29 +23,45 @@ import android.util.Log;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
-public class FacilityListActivity extends ListActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
 
-	public ArrayAdapter mAdapter;
+/**
+ * The FacilityListActivity displays a list of facilities pulled from the API.
+ * @author jmw
+ *
+ */
+public class FacilityListActivity extends ListActivity implements
+	GooglePlayServicesClient.ConnectionCallbacks,
+	GooglePlayServicesClient.OnConnectionFailedListener {
+
+	// A container class for a collection of Facility POJOs
+	static class FacilityList extends ArrayList<Facility> {}
+	
+	public FacilityArrayAdapter mAdapter;
+	private LocationClient mLocationClient;
+	private Location mCurrentLocation;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Create an empty adapter we will use to display the loaded data.
-		mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+		mAdapter = new FacilityArrayAdapter();
+		
+		mLocationClient = new LocationClient(this, this, this);
+		mLocationClient.connect();
+		
 		setListAdapter(mAdapter);
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
-		Toast.makeText(getApplication(), "Fetching Facilities...",
-				Toast.LENGTH_SHORT).show();
-		new HttpRequestTask().execute();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.facility_list, menu);
 		return true;
@@ -65,7 +74,7 @@ public class FacilityListActivity extends ListActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_refresh) {
-			new HttpRequestTask().execute();
+//			new HttpRequestTask().execute();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -77,19 +86,33 @@ public class FacilityListActivity extends ListActivity {
 	 * 
 	 * @author jmw
 	 */
-	private class HttpRequestTask extends AsyncTask<Void, Void, Facility[]> {
+	private class HttpRequestTask extends AsyncTask<Void, Void, List<Facility>> {
+		private Double lat, lng, rad;
+		
+		public HttpRequestTask(Double lat, Double lng, Double rad) {
+			super();
+			
+			this.lat = lat;
+			this.lng = lng;
+			this.rad = rad;	
+		}
+		
 		@Override
-		protected Facility[] doInBackground(Void... params) {
+		protected List<Facility> doInBackground(Void... params) {
 			try {
 				// final String url =
 				// "http://rest-service.guides.spring.io/greeting";
-				final String url = "http://10.88.0.108:3000/api/v1/Facilities/";
+				final String url = "http://192.168.1.86:3000/api/test/facilities/geowithin?lat=" 
+						+ this.lat + "&lng=" + this.lng + "&rad=" + this.rad;
+				
+				Log.i("FacilityListActivity", url);
+				
 				RestTemplate restTemplate = new RestTemplate();
 				restTemplate.getMessageConverters().add(
 						new MappingJackson2HttpMessageConverter());
 				
-				Facility[] facilities = restTemplate.getForObject(url,
-						Facility[].class);
+				List<Facility> facilities = restTemplate.getForObject(url,
+						FacilityList.class);
 
 				return facilities;
 			} catch (Exception e) {
@@ -100,9 +123,9 @@ public class FacilityListActivity extends ListActivity {
 		}
 
 		@Override
-		protected void onPostExecute(Facility[] facilities) {
-			Log.i("FacilityListActivity", ""+facilities.length);
-			Log.i("FacilityListActivity", ""+facilities[0].getName());
+		protected void onPostExecute(List<Facility> facilities) {
+			Log.i("FacilityListActivity", ""+facilities.size());
+//			Log.i("FacilityListActivity", ""+facilities[0].getName());
 			mAdapter.clear();
 			mAdapter.addAll(facilities);
 			mAdapter.notifyDataSetChanged();
@@ -115,29 +138,78 @@ public class FacilityListActivity extends ListActivity {
 	 * @author jmw
 	 *
 	 */
-	private class FacilityArrayAdapter extends ArrayAdapter<String> {
-
-		HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-		public FacilityArrayAdapter(Context context, int textViewResourceId,
-				List<String> objects) {
-			super(context, textViewResourceId, objects);
-			for (int i = 0; i < objects.size(); ++i) {
-				mIdMap.put(objects.get(i), i);
+	private class FacilityArrayAdapter extends ArrayAdapter<Facility> {
+		
+		public FacilityArrayAdapter() {
+			super(getApplicationContext(), 0);
+		}
+		
+		@Override
+		public View getView(int pos, View baseView, ViewGroup parent) {
+			if (baseView == null) {
+				baseView = getLayoutInflater().inflate(R.layout.facility_list_item, null);
 			}
+			
+			Facility f = getItem(pos);
+			
+			String type = (String) f.properties.get("type");
+			
+			ImageView icon = (ImageView) baseView.findViewById(R.id.facility_type_icon);
+			if (type.equals("health")) {
+				icon.setImageResource(R.drawable.hospital);				
+			} else if (type.equals("power")) {
+				icon.setImageResource(R.drawable.power);
+			} else if (type.equals("education")) {
+				icon.setImageResource(R.drawable.education);
+			}
+			
+			TextView title = (TextView) baseView.findViewById(R.id.facility_list_item_title);
+			title.setText(f.name);
+			
+			TextView desc = (TextView) baseView.findViewById(R.id.facility_list_item_description);
+			desc.setText("checkins: " + f.properties.get("checkins"));
+			
+//			TextView created = (TextView) baseView.findViewById(R.id.facility_list_created);
+//			created.setText("Facility added: " + f.createdAt.toString());
+			
+			Log.i("FacilityListActivity", f.properties.get("type")+"");
+			Log.i("FacilityListActivity", f.name);
+			
+			return baseView;
+			
 		}
+	}
+	
+	
+	
+	/********
+	 * EVENT HANDLERS
+	 ********/
 
-		@Override
-		public long getItemId(int position) {
-			String item = getItem(position);
-			return mIdMap.get(item);
-		}
+	@Override 
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Facility f = (Facility) getListView().getItemAtPosition(position);
+        Log.i("FacilityListActivity", "clicked " + f.name);
+    }
+	
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "GPS Connected", Toast.LENGTH_SHORT).show();;
+		mCurrentLocation = mLocationClient.getLastLocation();
+		new HttpRequestTask(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(), 1.0).execute();
+	}
 
-		@Override
-		public boolean hasStableIds() {
-			return true;
-		}
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/org/columbia/sel/facilitator/model/Facility.java
+++ src/org/columbia/sel/facilitator/model/Facility.java
@@ -1,22 +1,63 @@
 package org.columbia.sel.facilitator.model;
 
+import java.util.Date;
+import java.util.Map;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * A POJO representing a Facility.
+ * @author jmw
+ *
+ */
+
+// This tells Jackson to ignore extra keys in the JSON that's being mapped.
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Facility {
-    private String _id;
-    private String name;
-
-    public String getId() {
-        return this._id;
-    }
-
-    public String getName() {
-        return this.name;
+public class Facility implements Parcelable {
+    public String name;
+    
+    public String uuid;
+    
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="EST")
+    public Date createdAt;
+    // 2014-04-23T14:04:52.221Z
+    
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", timezone="EST")
+    public Date updatedAt;
+    
+    public Map<String, Object> properties;
+    
+    
+    
+    // Dummy constructor so that the Parcel constructor isn't used by default
+    private Facility() {
     }
     
-    public String toString() {
-    	return this.name;
+    public int describeContents() {
+        return 0;
     }
+
+    public void writeToParcel(Parcel out, int flags) {
+        //out.writeInt();
+    }
+    
+    public static final Parcelable.Creator<Facility> CREATOR = new Parcelable.Creator<Facility>() {
+		public Facility createFromParcel(Parcel in) {
+		    return new Facility(in);
+		}
+		
+		public Facility[] newArray(int size) {
+		    return new Facility[size];
+		}
+	};
+	
+	private Facility(Parcel in) {
+		//mData = in.readInt();
+	}
+	
 
 }
