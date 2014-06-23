80d6ae9ce37abb120b01bfd4fdd8359fecf66bae
Jonathan Wohl <jon@jonwohl.com>
2014-06-23 10:30:51 -0400

    reorganized tile packager

--- src/org/columbia/sel/facilitator/grout/OSMMapTilePackager.java
+++ src/org/columbia/sel/facilitator/grout/OSMMapTilePackager.java
@@ -91,6 +91,69 @@ public class OSMMapTilePackager {
 		return true;
 	}
 
+	/**
+	 * Kick off the process of downloading tiles.
+	 */
+	public void run() {
+		// remove previously cached files
+		String fullTempPath = this.mRootDownloadDir + "/" + this.mTempFolder;
+		String fullDestinationFilePath = this.mRootDownloadDir + "/" + this.mDestinationFile;
+
+		Log.i(TAG, "----------------------- DELETING TILES in " + fullTempPath);
+		Util.deleteDirectory(new File(fullTempPath));
+
+		// If region is valid for download, do it.
+		if (isValidRegionForDownload()) {
+			Log.i(TAG, "----------------------- DOWNLOADING TILES in " + this.mTempFolder);
+			downloadTiles(mServerURL, fullTempPath, mThreadCount, mFileAppendix, mMinZoom, mMaxZoom, mNorth, mSouth,
+					mEast, mWest);
+		} else {
+			
+		}
+
+		if (mDestinationFile != null) {
+			if (mDestinationFile.endsWith(".zip")) {
+				Log.i(TAG, "----------------------- ZIPPING TILES in " + this.mTempFolder);
+				runZipToFile(fullTempPath, fullDestinationFilePath);
+			} else if (mDestinationFile.endsWith(".gemf")) {
+				runCreateGEMFFile(fullTempPath, fullDestinationFilePath);
+			} else {
+				runCreateDb(fullTempPath, fullDestinationFilePath);
+			}
+
+			Log.i(TAG, "---------------------------");
+
+			if (mServerURL != null) {
+				runCleanup(fullTempPath);
+			}
+		}
+	}
+
+
+	private void runFileExistenceChecker(final int pExpectedFileCount, final String pTempFolder, final int pMinZoom,
+			final int pMaxZoom, final double pNorth, final double pSouth, final double pEast, final double pWest) {
+
+		// abortIfUserIsNotSure("This will check if the actual filecount is the same as the expected ("
+		// + pExpectedFileCount + ").");
+
+		/* Quickly count files in the tempFolder. */
+		Log.i(TAG, "Counting existing files ...");
+		final int actualFileCount = FolderFileCounter.getTotalRecursiveFileCount(new File(pTempFolder));
+		if (pExpectedFileCount == actualFileCount) {
+			Log.i(TAG, " done.");
+		} else {
+			Log.i(TAG, " FAIL!");
+			// abortIfUserIsNotSure("Reason: Actual files:" + actualFileCount +
+			// "    Expected: " + pExpectedFileCount + ". Proceed?");
+		}
+	}
+
+	
+	
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+	
 	public String getServerURL() {
 		return mServerURL;
 	}
@@ -186,228 +249,8 @@ public class OSMMapTilePackager {
 	public void setThreadCount(int threadCount) {
 		this.mThreadCount = threadCount;
 	}
-
-	public void start(final String[] args) {
-		if (args == null || args.length == 0) {
-			printUsageAndExit();
-		}
-
-		/* Parsing will only start if this variable was set. */
-		FORCE = false;
-		String serverURL = null;
-		String destinationFile = null;
-		String tempFolder = null;
-		String fileAppendix = "";
-		Double north = null;
-		Double south = null;
-		Double east = null;
-		Double west = null;
-		Integer maxzoom = null;
-		int minzoom = 0;
-		int threadCount = DEFAULT_THREADCOUNT;
-
-		try {
-			for (int i = 0; i < args.length; i += 2) {
-				if (args[i].equals("-u")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						serverURL = args[i + 1];
-					}
-				} else if (args[i].equals("-force")) {
-					i--;
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						FORCE = true;
-					}
-				} else if (args[i].equals("-d")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						destinationFile = args[i + 1];
-					}
-				} else if (args[i].equals("-fa")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						fileAppendix = args[i + 1];
-					}
-				} else if (args[i].equals("-nthreads")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						threadCount = Integer.parseInt(args[i + 1]);
-					}
-				} else if (args[i].equals("-zmin")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						minzoom = Integer.parseInt(args[i + 1]);
-					}
-				} else if (args[i].equals("-zmax")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						maxzoom = Integer.parseInt(args[i + 1]);
-					}
-				} else if (args[i].equals("-t")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						tempFolder = args[i + 1];
-					}
-				} else if (args[i].equals("-n")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						north = Double.parseDouble(args[i + 1]);
-					}
-				} else if (args[i].equals("-s")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						south = Double.parseDouble(args[i + 1]);
-					}
-				} else if (args[i].equals("-e")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						east = Double.parseDouble(args[i + 1]);
-					}
-				} else if (args[i].equals("-w")) {
-					if (i >= args.length) {
-						printUsageAndExit();
-					} else {
-						west = Double.parseDouble(args[i + 1]);
-					}
-				}
-			}
-		} catch (final NumberFormatException nfe) {
-			printUsageAndExit();
-		}
-
-		if (tempFolder == null) {
-			printUsageAndExit();
-		}
-
-		if (serverURL == null && !new File(tempFolder).exists()) {
-			printUsageAndExit();
-		}
-
-		if (north == null || south == null || east == null || west == null) {
-			printUsageAndExit();
-		}
-
-		run(serverURL, destinationFile, tempFolder, threadCount, fileAppendix, minzoom, maxzoom, north, south, east,
-				west);
-	}
-
-	public void run() {
-		// remove previously cached files
-		String fullTempPath = this.mRootDownloadDir + "/" + this.mTempFolder;
-		String fullDestinationFilePath = this.mRootDownloadDir + "/" + this.mDestinationFile;
-
-		Log.i(TAG, "----------------------- DELETING TILES in " + fullTempPath);
-		Util.deleteDirectory(new File(fullTempPath));
-
-		// If region is valid for download, do it.
-		if (isValidRegionForDownload()) {
-			Log.i(TAG, "----------------------- DOWNLOADING TILES in " + this.mTempFolder);
-			downloadTiles(mServerURL, fullTempPath, mThreadCount, mFileAppendix, mMinZoom, mMaxZoom, mNorth, mSouth,
-					mEast, mWest);
-		}
-
-		if (mDestinationFile != null) {
-			if (mDestinationFile.endsWith(".zip")) {
-				Log.i(TAG, "----------------------- ZIPPING TILES in " + this.mTempFolder);
-				runZipToFile(fullTempPath, fullDestinationFilePath);
-			} else if (mDestinationFile.endsWith(".gemf")) {
-				runCreateGEMFFile(fullTempPath, fullDestinationFilePath);
-			} else {
-				runCreateDb(fullTempPath, fullDestinationFilePath);
-			}
-
-			Log.i(TAG, "---------------------------");
-
-			if (mServerURL != null) {
-				runCleanup(fullTempPath);
-			}
-		}
-
-	}
-
-	private void run(final String pServerURL, final String pDestinationFile, final String pTempFolder,
-			final int pThreadCount, final String pFileAppendix, final int pMinZoom, final int pMaxZoom,
-			final double pNorth, final double pSouth, final double pEast, final double pWest) {
-
-		if (pServerURL != null) {
-			Log.i(TAG, "--------------------------- DELETING TILES in " + pTempFolder);
-			Util.deleteDirectory(new File(pTempFolder));
-
-			Log.i(TAG, "---------------------------");
-			final int expectedFileCount = getExpectedFileCount(pMinZoom, pMaxZoom, pNorth, pSouth, pEast, pWest);
-
-			Log.i(TAG, "---------------------------");
-			downloadTiles(pServerURL, pTempFolder, pThreadCount, pFileAppendix, pMinZoom, pMaxZoom, pNorth, pSouth,
-					pEast, pWest);
-
-			Log.i(TAG, "---------------------------");
-			runFileExistenceChecker(expectedFileCount, pTempFolder, pMinZoom, pMaxZoom, pNorth, pSouth, pEast, pWest);
-		}
-
-		if (pDestinationFile != null) {
-			Log.i(TAG, "---------------------------");
-			if (pDestinationFile.endsWith(".zip")) {
-				runZipToFile(pTempFolder, pDestinationFile);
-			} else if (pDestinationFile.endsWith(".gemf")) {
-				runCreateGEMFFile(pTempFolder, pDestinationFile);
-			} else {
-				runCreateDb(pTempFolder, pDestinationFile);
-			}
-
-			Log.i(TAG, "---------------------------");
-
-			if (pServerURL != null) {
-				runCleanup(pTempFolder);
-			}
-		}
-
-		Log.i(TAG, "---------------------------");
-	}
-
-	private void runFileExistenceChecker(final int pExpectedFileCount, final String pTempFolder, final int pMinZoom,
-			final int pMaxZoom, final double pNorth, final double pSouth, final double pEast, final double pWest) {
-
-		// abortIfUserIsNotSure("This will check if the actual filecount is the same as the expected ("
-		// + pExpectedFileCount + ").");
-
-		/* Quickly count files in the tempFolder. */
-		Log.i(TAG, "Counting existing files ...");
-		final int actualFileCount = FolderFileCounter.getTotalRecursiveFileCount(new File(pTempFolder));
-		if (pExpectedFileCount == actualFileCount) {
-			Log.i(TAG, " done.");
-		} else {
-			Log.i(TAG, " FAIL!");
-			// abortIfUserIsNotSure("Reason: Actual files:" + actualFileCount +
-			// "    Expected: " + pExpectedFileCount + ". Proceed?");
-		}
-	}
-
-	private void printUsageAndExit() {
-		String message = "Usage:\n" + "-u\t[OSM-style tile URL: http://_URL_/%d/%d/%d.png]\n"
-				+ "-t\t[Temporary Folder]\n" + "-d\t[Destination-file: C:\\mappack.zip]\n"
-				+ "-zmin\t[Minimum zoomLevel to download. Default: 0]\n" + "-zmax\t[Maximum zoomLevel to download]\n"
-				+ "-fa\t[Filename-Appendix. Default: \"\"]\n" + "-n\t[North Latitude]\n" + "-s\t[South Latitude]\n"
-				+ "-e\t[East Longitude]\n" + "-w\t[West Longitude]\n"
-				+ "-nthreads\t[Number of Download-Threads. Default: 2]\n";
-		Log.i(TAG, message);
-	}
-
-	// ===========================================================
-	// Getter & Setter
-	// ===========================================================
-
+	
+	
 	// ===========================================================
 	// Methods from SuperClass/Interfaces
 	// ===========================================================
